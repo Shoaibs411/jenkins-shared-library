d502a3069a712a639cacf7bc03a769883ecd130e
Shoaib <shoaibs411@gmail.com>
2023-12-17 00:57:38 +0530

    Jenkins-shared-library AutoPush

---
+++ vars/nodejs.groovy
@@ -0,0 +1,7 @@
+def lintChecks(){
+                sh "echo ***** Starting Style Checks ***** "
+                sh "npm install jslint"
+                sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
+                sh "echo ***** Style Checks are completed ***** "
+
+}
