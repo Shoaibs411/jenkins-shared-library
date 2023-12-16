e519f13ed8c46179ef1e7c33df7203ef22ed18bf
Shoaib <shoaibs411@gmail.com>
2023-12-17 02:04:05 +0530

    Jenkins-shared-library AutoPush

--- vars/python.groovy
+++ vars/python.groovy
@@ -3,7 +3,8 @@ def lintChecks_python(component){
         //sh "pip install pylint"
         //sh "/home/centos/payment/pylint payment.py || true"
         //sh "/home/centos/payment/pylint rabbitmq.py || true"
-        sh "/home/centos/payment/ pylint *.py || true"
+        //sh "/home/centos/payment/ pylint *.py || true"
+        sh "pylint ~/payment/*.py"
         sh "echo ***** Style Checks are completed for ${component}  ***** "
 
 } 
