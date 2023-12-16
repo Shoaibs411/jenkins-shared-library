f43260414392c66b9cea4ed5254356ff9358df47
Shoaib <shoaibs411@gmail.com>
2023-12-17 00:42:02 +0530

    Jenkins-shared-library AutoPush

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,8 +1,8 @@
 // info is the default function will be called when I call the variable
 
 
-def info(msg){
-    echo "INFO : Trying demo on Shared Library with ${msg}"
+def info(message){
+    echo "INFO : Trying demo on Shared Library with ${message}"
 }
 
-info("catalogue")
+info("Catalogue")
