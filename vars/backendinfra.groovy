17c9bad94fb8ec0314471ec0a65816879a66729c
Shoaib <shoaibs411@gmail.com>
2024-01-18 01:42:59 +0530

    Creating Mutable App - TF AutoPush - Configuring Cart

---
+++ vars/backendinfra.groovy
@@ -0,0 +1,67 @@
+
+def call(){
+    properties([
+        parameters([
+            choice(choices: ['dev\nprod', description: 'Select the Environment', name: "ENV"])
+            choice(choices: ['apply\ndestroy', description: 'Select an Action to perform', name: "ACTION"])
+        ]),
+    ])
+    node('ws') {
+        ansiColor('xterm'){
+            git branch: 'main', url: "https://github.com/Shoaibs411/${COMPONENT}.git"
+            stage('Terraform Init'){
+                sh 
+                '''
+                    cd mutable-infra
+                    terrafile -f env-dev/Terrafile
+                    terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars
+                '''
+            }
+            stage('Terraform Plan'){
+                sh 
+                '''
+                    cd mutable-infra
+                    terraform plan -var-file=env-${ENV}/${ENV}.tfvars
+                '''
+            }
+            stage('Terraform Action'){
+                sh 
+                '''
+                    cd mutable-infra
+                    terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars
+                '''
+            }
+            
+        }
+    }
+
+// pipeline {
+//     agent{
+//         label 'ws'
+//     }
+//     options {
+//         ansiColor('xterm')
+//     }
+//     parameters {
+//         choice(name: 'ENV', choices: ['dev', 'prod'], description: 'Select the environment')
+//         choice(name: 'ACTION', choices: ['apply', 'destroy'], description: 'Select apply or destroy')
+//     }
+//     stages {
+//         stage('Terraform Init') {
+//             steps {
+//                 sh "terrafile -f env-dev/Terrafile"
+//                 sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
+//             }
+//         }
+//         stage('Terraform Plan') {
+//             steps {
+//                 sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+//             }
+//         }
+//         stage('Terraform Action') {
+//             steps {
+//                 sh "terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars"
+//             }
+//         }
+//     }
+// }
